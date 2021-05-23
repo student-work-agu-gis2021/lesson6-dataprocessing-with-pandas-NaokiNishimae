1ace18a7ee5a5bc9b78b9c43cec63df7f5c60d96
NaokiNishimae <>
2021-05-23 18:00:37 +0000

    I added a function for the calculation of meteorological data

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -17,7 +17,11 @@ import numpy as np
 data = None
 
 # YOUR CODE HERE 1
+fp="data/1091402.txt"
+data=pd.read_csv(fp,delim_whitespace=True,skiprows=[1],na_values=[-9999])
 
+print(data.head())
+print(data.tall())
 # ### Part 2 
 # 
 # In this section, you will calculate simple statistics based on the input data:
@@ -27,7 +31,7 @@ data = None
 
 tavg_nodata_count = None
 #YOUR CODE HERE 2
-
+tavg_nodata_count=data.iloc[:,6].isnull().sum()
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -40,6 +44,7 @@ print('Number of no-data values in column "TAVG":',tavg_nodata_count)
 
 tmin_nodata_count = None
 #YOUR CODE HERE 3
+tmin_nodata_count=data.iloc[:,8].isnull().sum()
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -52,6 +57,7 @@ print('Number of no-data values in column "TMIN":', tmin_nodata_count)
 
 day_count = None 
 #YOUR CODE HERE 4
+day_count=len(data)
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -65,6 +71,7 @@ print("Number of days:", day_count)
 first_obs = None
  
 # YOUR CODE HERE 5
+first_obs=data.iloc[0,4]
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -77,7 +84,7 @@ print('Date of the first observation:',first_obs)
 last_obs = None
 
 # YOUR CODE HERE 6
-
+last_obs=data.iloc[day_count-1.4]
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print('Date of the last observation:', last_obs)
@@ -90,7 +97,7 @@ print('Date of the last observation:', last_obs)
 avg_temp = None
 
 # YOUR CODE HERE 7
-
+avg_temp=np.mean(data.iloc[:,6])
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print('Average temperature (F) for the whole dataset:', round(avg_temp, 2))
@@ -103,7 +110,7 @@ print('Average temperature (F) for the whole dataset:', round(avg_temp, 2))
 avg_temp_1969 = None
 
 # YOUR CODE HERE 8
-
+avg_temp_1969=data['TMAX'].loc[(data['DATE']>=199690501)&(data['DATE']<19690901)].mean()
 #CAUTION!!! DON'T EDIT THIS PART START
 # This test print should print a number
 print('Average temperature (F) for the Summer of 69:', round(avg_temp_1969, 2))
